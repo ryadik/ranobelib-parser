ca04c632854165be5c87c3cb17396f9c5061dd1c
K1nGsmaN-hub <15162342h@gmail.com>
2023-09-26 13:16:06 +0800

    feat: add test file

---
+++ test.js
@@ -0,0 +1,9 @@
+import promptSync from "prompt-sync";
+
+const prompt = promptSync({ sigint: true });
+
+console.log('test input')
+
+const testInput = prompt();
+
+console.log(`testInput: ${testInput}`)
